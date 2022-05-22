bf8657eabf809a6e8274fc6119ddd46e0bbccaa0
japesjake <japesjake@gmail.com>
2022-05-21 21:44:04 -0400

    added three window to gui.py

---
+++ background.py
@@ -0,0 +1,45 @@
+class Background:
+    def __init__(self):
+        self.venues = list()
+    def add_venue(self, venue):
+        self.venues.append(venue)
+
+class Venue():
+    def __init__(self, name):
+        self.name = name
+        self.venues = list()
+        self.tables = list()
+        self.groups = list()
+    def add_table(table):
+        self.tables.add(table)
+    def set_name(name):
+        self.name = name
+    def get_name():
+        return self.name
+
+class Group():
+    def __init__(self):
+        self.persons = list()
+    def get_num_persons():
+        return len(self.persons)
+    def add_person(person):
+        self.persons.add(person)
+    
+class Table():
+    def __init__(self, name):
+        self.name = name
+        self.persons = list()
+    def set_name(self, name):
+        self.name = name
+    def get_name(self):
+        return self.name
+    def get_num_persons(self):
+        return len(self.persons)
+    def add_person(self, person):
+        self.persons.add(person)
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age              # to do: assign persons of similar age group to same tables
+                                    # to do: make list of persons not allowed at the same table
--- gui.py
+++ gui.py
@@ -1,45 +1,57 @@
 
-from logging import root
+from doctest import master
 import tkinter as tk
-import venue
+import background as bg
 
 class Gui:
     def __init__(self):
-        self.introduce()
-    def newWindow(self):
         self.window = tk.Tk()
-        self.window.title("Venue Planner")
-        self.window.geometry("1920x1080")
+        self.introduce()
 
+    def getWindow(self):
+        return self.window
+    # Assigns shorcut key "Return" to function which opens the next window
+    def linkReturn(self, func):
+        self.window.bind("<Return>", func)
+    #closes current window and opens the next window. also, runs function ^above^
+    def newWindow(self, func):
+        if self.window: self.window.destroy()
+        self.window = tk.Tk()
+        self.window.configure(bg = "blue")
+        self.window.title("Venue Planner")
+        self.window.geometry("800x600")
+        self.linkReturn(func)
+
+    #made methods for creating widgets so I don't have to type "text =, bg =" etc.
+    def button(self, command, padx = 0, pady = 0, bg = "blue", fg = "blue"):
+        tk.Button(self.window, command = command, padx = padx, pady = pady, bg = bg, fg = fg)
+    
+    def label(self, text, padx = 0, pady = 0, bg = "blue", fg = "white"):
+        tk.Label(self.window, text = text, padx = padx, pady = pady, bg = bg, fg = fg).pack()
+    
+    def entry(self, text,  padx = 0, pady = 0, bg = "blue", fg = "white"):
+        tk.Entry(self.window, text = text, padx = padx, pady = pady, bg = bg, fg = fg).pack()
+    
+    ## WINDOW FUNCTIONS ##
+    
     # Displays opening message
     def introduce(self):
-        self.newWindow()
-        # self.window = tk.Tk()
-        # self.window.title("Venue Planner")
-        # self.window.geometry("1920x1080")
-        frame = tk.Frame()
-        tk.Label(frame, 
-            padx = 50,
-            text = "Welcome to Venue Planner!"
-        ).pack()
-        tk.Label(
-            pady = 50,
-            text = "This application automatically assigns seats based on information provided."
-        ).pack()
-        tk.Label(
-            text = "press the button to begin:"
-        ).pack()
-        tk.Button(
-            text = "begin",
-            command = self.query1
-            ).pack()
-        tk.mainloop()
+        self.newWindow(self.query1)
+        self.label("Welcome to Venue Planner!", 0, 50)
+        self.label("This application automatically assigns your guests to seats based on information provided.", self.window, 0, 50)
+        self.label("Press enter or click on the button to begin:", 0, 25)
+        self.button(self.query1)
+        self.window.mainloop()
     # querys for name of venue
-    def query1(self):
-        self.window.withdraw()
-        self.newWindow()
-
+    def query1(self, event = None):
+        self.newWindow(self.query2)
+        self.label("Please enter the name of your venue:", self.window)
+        self.button("continue", self.query2, self.window)
+    # querys for number of tables and max seats per table
+    def query2(self, event = None):
+        self.newWindow(self.query3)
+    def query3(self):
+        pass
 if __name__ == "__main__":
     gui = Gui()
-    global venue
-    venue = venue()
+    bg = bg()
